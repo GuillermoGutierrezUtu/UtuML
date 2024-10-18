5b7fbc0930ce0aec42a0adc80a29b797f6a13490
Lab5 <Lab5@LAB4-PC10>
2024-10-18 20:59:06 -0300

    Agregamos Cedula por teclado

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -3,17 +3,23 @@ package javaapplication3;
 import java.util.*;
 
 public class JavaApplication3 {
-    
+
     public static void main(String[] args) {
-        
-        ArrayList<Integer> ci = new ArrayList<Integer>();        
+
+        ArrayList<Integer> ci = new ArrayList<Integer>();
         ci.add(55394567);
         System.out.println("Cedula ingresada: " + ci.get(0));
-        
-        
-        
-        
-        
+        Scanner Scanner = new Scanner(System.in);
+        int cedula;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese su cedula: ");
+            cedula = Scanner.nextInt();
+            ci.add(cedula);
+        }
+
+        for (int i = 0; i < ci.size(); i++) {
+            System.out.println(" Cedula ingresada " + ci.get(i));
+        }
+
     }
-    
 }
