9da4ab4bcce9fb73dc19230f76b1433ec8552a1e
Lab5 <Lab5@LAB4-PC10>
2024-10-18 20:39:26 -0300

    Subida inicial.

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,19 @@
+package javaapplication3;
+
+import java.util.*;
+
+public class JavaApplication3 {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> ci = new ArrayList<Integer>();        
+        ci.add(55394567);
+        System.out.println("Cedula ingresada: " + ci.get(0));
+        
+        
+        
+        
+        
+    }
+    
+}
